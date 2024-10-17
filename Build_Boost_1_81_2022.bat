013bccb7c2be71010b81af126f5ca8a0e708b57f
Glen Johnson <glen.johnson@gaj-geospatial.com>
2024-10-17 15:05:29 -0400

    initial check in

---
+++ Build_Boost_1_81_2022.bat
@@ -0,0 +1,8 @@
+cd
+if not exist b2.exe call bootstrap.bat msvc vc143
+b2.exe --toolset=msvc-14.3 address-model=64 --build-type=complete
+if not exist stage\\x64\ mkdir stage\\x64
+if not exist stage\\x64\\lib\ move stage\\lib stage\\x64\\lib
+if not exist stage\\lib\ mkdir stage\\lib
+if exist stage\\x64\\lib\\cmake\ move stage\\x64\\lib\\cmake stage\\lib\\cmake
+
