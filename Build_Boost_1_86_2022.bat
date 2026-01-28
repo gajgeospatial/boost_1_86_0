a2d76e1b38a3c255c389ca159192d11cd23a8a96
Glen A Johnson <glen.johnson@gaj-geospatial.com>
2026-01-28 16:05:45 -0500

    remove python libraries from build

--- Build_Boost_1_86_2022.bat
+++ Build_Boost_1_86_2022.bat
@@ -1,6 +1,6 @@
 cd
 if not exist b2.exe call bootstrap.bat msvc vc143
-b2.exe --toolset=msvc-14.3 address-model=64 --build-type=complete
+b2.exe --without-python --toolset=msvc-14.3 address-model=64 --build-type=complete
 if not exist stage\\x64\ mkdir stage\\x64
 if not exist stage\\x64\\lib\ move stage\\lib stage\\x64\\lib
 if not exist stage\\lib\ mkdir stage\\lib
